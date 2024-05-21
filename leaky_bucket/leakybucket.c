63de9824efa09bae8edf9ca9c6ac78191c7b7688
Namitha S <114816602+Namitha-KS@users.noreply.github.com>
2024-05-21 11:05:28 +0530

    Add files via upload

---
+++ leaky_bucket/leakybucket.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main() {
+    int in, leakrate, bsize, n, bucket_storage;
+    bucket_storage = 0;
+    printf("Enter the bucket size: ");
+    scanf("%d", &bsize);
+    printf("Enter the no of inputs: ");
+    scanf("%d", &n);
+    printf("Enter the packet outgoing rate: ");
+    scanf("%d", &leakrate);
+    printf("Initially Bucket status: contains %d out of %d \n", bucket_storage, bsize);
+    while (n != 0) 
+    {
+        printf("\nEnter the incoming packet size: ");
+        scanf("%d", &in);
+        printf("Incoming packet size is %d \n", in);
+        if (in <= (bsize - bucket_storage)) 
+        {
+            bucket_storage += in;
+            printf("Bucket status: contains %d out of %d \n", bucket_storage, bsize);
+        } 
+        else 
+        {
+            printf("Dropped packets: %d \n", in - (bsize - bucket_storage));
+            bucket_storage = bsize;
+            printf("Bucket status: contains %d out of %d\n", bucket_storage, bsize);
+        }
+        n--;
+    }
+    if (n == 0 && bucket_storage != 0) 
+    {
+        bucket_storage = 0;
+        printf("After sending, Bucket contains %d packets out of %d\n", bucket_storage, bsize);
+    }
+}
+
